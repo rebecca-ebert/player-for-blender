c97020f883db304331bf8d2acea2c14e2e5343d3
abc013 <abc013@alpenjodel.de>
2023-12-28 19:03:15 +0100

    Get plugin to work again
    Signed-off-by: abc013 <abc013@alpenjodel.de>

--- __init__.py
+++ __init__.py
@@ -371,7 +371,8 @@ def select_plant(filepath, defaults=False):
     old_qual = tp.qualifier
 
     if defaults:
-        for key in tp.keys():
+        keys = list(tp.keys())
+        for key in keys:
             tp.pop(key)
 
     tp.name = plant.name
